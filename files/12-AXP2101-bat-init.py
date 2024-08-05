9f37ee9d8ba1e397250cca79439a796f3dbc3c24
Bill Sideris <bill88t@feline.gr>
2024-08-06 01:29:36 +0300

    Add touchscreen, support more properties on bat

--- files/12-AXP2101-bat-init.py
+++ files/12-AXP2101-bat-init.py
@@ -14,6 +14,27 @@ class battery:
     def percentage(self) -> int:
         return max(0, min(100, self._pmic.battery_level if self._pmic.is_battery_connected else 0))
 
+    @property
+    def charger(self) -> bool:
+        return
+
+    @property
+    def status(self) -> bool:
+        if self._pmic.battery_status.value == 2:
+            return "charging"
+        elif self._pmic.battery_status.value == 1:
+            return "charged"
+        else:
+            return "discharging"
+
+    @property
+    def charging_enabled(self) -> bool:
+        return self._pmic.battery_charging_enabled
+
+    @charging_enabled.setter
+    def charging_enabled(self, value: bool) -> None:
+        self._pmic.battery_charging_enabled = value
+
 vr("axp", be.devices["AXP2101"][0])
 be.devices[vr("dev_name")][vr("dev_id")] = battery(vr("axp"))
 del battery
